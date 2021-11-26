625fbf7cdc4fc5f1191c0e15493fef0b70123a26
Rubber T. Dork <hi@rubberdork.com>
2021-11-26 13:18:43 +0800

    Validate addresses; send error reply if invalid

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import { Client, Intents } from 'discord.js'
-import { Asset } from 'stellar-sdk'
+import { Networks, Server, BASE_FEE, Asset } from 'stellar-sdk'
+
+import { accountValidator } from './lib/account_validator.js'
+
 
 const {
+  NODE_ENV,
   DISCORD_CLIENT_ID,
   DISCORD_TOKEN,
   DISCORD_GUILD_ID,
   CHANNEL_ID_REGISTER,
+  ASSET_CODE,
+  ASSET_ISSUER
 } = process.env
 
+const asset = new Asset(ASSET_CODE, ASSET_ISSUER)
+const testnet = NODE_ENV === 'development'
+const NETWORK = testnet ? 'TESTNET' : 'PUBLIC'
+const networkPassphrase = Networks[NETWORK]
+const HORIZON_URL = `https://horizon${testnet ? '-testnet' : ''}.stellar.org`
+const TX_URL = `https://stellar.expert/explorer/${testnet ? 'testnet' : 'public'}/tx`
+const server = new Server(HORIZON_URL)
+const txnOpts = {
+  fee: BASE_FEE,
+  networkPassphrase,
+}
+const validateAccount = accountValidator(server, asset)
 const bot = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
@@ -31,7 +49,15 @@ bot.on('messageCreate', async (msg) => {
   if (channelId !== CHANNEL_ID_REGISTER || author.id === DISCORD_CLIENT_ID) {
     return
   }
-  msg.reply('pong')
+
+  const address = await validateAccount(content)
+
+  if (!address.is_valid) {
+    const { id, username } = author
+    const date = new Date().toISOString()
+    console.log(`${date} - ${username} ${id} entered an invalid address: ${content} - ${address.reason}`)
+    return msg.reply(`${address.reason}. Please try a different Stellar address`)
+  }
 })
 
 bot.login(DISCORD_TOKEN)
