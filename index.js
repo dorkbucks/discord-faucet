d62f8cfea99f881e54eb44b451c832077015c300
Rubber T. Dork <hi@rubberdork.com>
2021-11-26 12:51:33 +0800

    Scaffold bot (pong!)

---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Client, Intents } from 'discord.js'
+import { Asset } from 'stellar-sdk'
+
+const {
+  DISCORD_CLIENT_ID,
+  DISCORD_TOKEN,
+  DISCORD_GUILD_ID
+} = process.env
+
+const bot = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
+})
+
+bot.once('ready', () => console.log(`Faucet bot logged in as ${DISCORD_CLIENT_ID}`))
+
+bot.on('messageCreate', async (msg) => {
+  const { channelId, content, author } = msg
+  if (author.id === DISCORD_CLIENT_ID) {
+    return
+  }
+  msg.reply('pong')
+})
+
+bot.login(DISCORD_TOKEN)
