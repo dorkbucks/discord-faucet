ef0f87d779a738afe567ab10002749b06c063d3f
Rubber T. Dork <hi@rubberdork.com>
2021-11-29 21:03:01 +0800

    Bump up the fee again

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const HORIZON_URL = `https://horizon${testnet ? '-testnet' : ''}.stellar.org`
 const TX_URL = `https://stellar.expert/explorer/${testnet ? 'testnet' : 'public'}/tx`
 const server = new Server(HORIZON_URL)
 const txnOpts = {
-  fee: 500,
+  fee: 1000,
   networkPassphrase,
 }
 
